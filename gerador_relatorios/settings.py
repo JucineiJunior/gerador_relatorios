06c726c0f3bbd1120000407932661d52775ddc79
Jucinei Junior <jucinei@redetrevo.com.br>
2025-08-18 11:35:53 -0300

    Remoção de securekey do github

--- gerador_relatorios/settings.py
+++ gerador_relatorios/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nt9%na9jcuj8fz_@z9n+srvy3ma03+!u%&3)#!-_f=1m18qpr)"
+load_dotenv()
+SECRET_KEY = os.getenv("SECURE_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
