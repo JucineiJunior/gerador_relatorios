9f9777a163e18fd029d3c2bbfece81c9f398886b
JucineiJr <jucinei@redetrevo.com.br>
2025-09-05 16:06:37 -0300

    Dev de agrupamentos, testes iniciais

--- usuario/models.py
+++ usuario/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from administrador.models import Perfil, Relatorios
+from administrador.models import Filtros, Perfil, Relatorios
 
 
 # Create your models here.
@@ -10,11 +10,18 @@ class Agendamentos(models.Model):
     relatorio = models.ForeignKey(
         Relatorios, on_delete=models.CASCADE, null=False, related_name="agendamento"
     )
-    metodo = models.CharField(choices=METODO_CHOICES)
-    intervalo = models.IntegerField(null=False, default=1)
-    usuario = models.ForeignKey(
-        Perfil, on_delete=models.CASCADE, null=False, name="usuario"
+    metodo = models.CharField(choices=METODO_CHOICES, max_length=255)
+    intervalo = models.IntegerField(null=False)
+    usuario = models.ForeignKey(Perfil, on_delete=models.CASCADE, name="usuario")
+    beneficiados = models.ManyToManyField(Perfil, related_name="beneficiarios")
+    filtros = models.ManyToManyField(Filtros, related_name="filtros")
+
+
+class FiltrosPreenchimento(models.Model):
+    agendamento = models.ForeignKey(
+        Agendamentos, on_delete=models.CASCADE, related_name="agendamento"
     )
-    beneficiados = models.ManyToManyField(
-        Perfil, null=False, related_name="beneficiarios"
+    filtro = models.ForeignKey(
+        Filtros, on_delete=models.CASCADE, related_name="parametros"
     )
+    preenchimento = models.CharField(max_length=255)
